b8583fe9ffd7178bdfbfd1afab2809aacbcefa8a
nicolasteixeira3856 <33877493+nicolasteixeira3856@users.noreply.github.com>
2019-04-08 19:21:03 -0300

    Curso PHP

---
+++ 10 - PHP7/data/echo_print.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+    </head>
+    <body>
+        <?php
+
+            echo 'Comando echo <br>';
+            print 'Comando print <hr>';
+        ?>
+    </body>
+</html>
