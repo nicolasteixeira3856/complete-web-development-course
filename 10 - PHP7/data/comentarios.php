25e019efaabfb21ee0680b1f3f985a53528d906f
nicolasteixeira3856 <33877493+nicolasteixeira3856@users.noreply.github.com>
2019-04-08 19:16:46 -0300

    Iniciando o curso de PHP7

---
+++ 10 - PHP7/data/comentarios.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Comentário de uma linha
+    #Comentário de uma linha
+    /*
+        Comentário de multiplas linhas
+        Este tipo de comentário permite que várias
+        linhas sejam comentadas ao mesmo tempo
+    */
+
+?>
