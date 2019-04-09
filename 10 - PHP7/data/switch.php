4e7af02abfeb7280a8aac25defff567a7b3de931
nicolasteixeira3856 <33877493+nicolasteixeira3856@users.noreply.github.com>
2019-04-09 16:48:00 -0300

    Create switch.php

---
+++ 10 - PHP7/data/switch.php
@@ -0,0 +1,19 @@
+<?php
+
+    $opcao = 1;
+    
+    switch ($opcao) {
+        case '1':
+            echo 'Valor 1';
+            break;
+        
+        case '2':
+            echo 'Valor 2';
+            break;
+
+        default:
+            echo 'Valor Default';
+            break;
+    }
+
+?>
